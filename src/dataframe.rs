5866f186d8d7cc43efd7af11dbc686732de023c8
pegasos1 <sgrangan01@gmail.com>
2016-11-15 01:49:13 -0800

    added btreemap, optimized, all test pass. also added drop, concat, and other functions

--- src/dataframe.rs
+++ src/dataframe.rs
@@ -1,5 +1,7 @@
 use ndarray::{Array, Ix, Axis, ArrayView, stack};
+use std::collections::BTreeMap;
 use std::collections::HashMap;
+
 use std::hash::Hash;
 use std::fmt::Debug;
 use num::Float;
@@ -16,30 +18,28 @@ pub type MatrixView<'a, T> = ArrayView<'a, T, (Ix, Ix)>;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct DataFrame<T: Float> {
-    data_map: HashMap<String, usize>,
-    inner_matrix: Matrix<T>,
+    pub data_map: BTreeMap<String, usize>,
+    pub inner_matrix: Matrix<T>,
 }
-
 
 impl<T: Float> DataFrame<T> {
     pub fn inner_matrix(&self) -> &Matrix<T> {
         &self.inner_matrix
     }
-    pub fn datamap(&self) -> &HashMap<String, usize> {
+    pub fn datamap(&self) -> &BTreeMap<String, usize> {
         &self.data_map
     }
 
-    pub fn new(data: Matrix<T>, names: Vec<String>) -> Result<DataFrame<T>, &'static str> {
+    pub fn new(data: Matrix<T>,
+               datamap: BTreeMap<String, usize>)
+               -> Result<DataFrame<T>, &'static str> {
 
-        if names.len() != data.shape()[1] {
+        if datamap.len() != data.shape()[1] {
             return Err("shape mismatch!");
         }
 
 
-        let datamap: HashMap<String, usize> = names.iter()
-            .enumerate()
-            .map(|(x, y)| (y.clone(), x))
-            .collect();
+
 
         let dm = DataFrame {
             data_map: datamap,
@@ -71,7 +71,97 @@ impl<T: Float> DataFrame<T> {
 
     }
 
-    pub fn inner_join(self, other: DataFrame<T>, on: &str) -> Result<DataFrame<T>, &'static str> {
+    pub fn shape(&self) -> (usize, usize) {
+        self.inner_matrix.dim()
+    }
+
+    pub fn merge_maps<K: Hash + Eq + Copy, V: Copy>(first_context: &HashMap<K, V>,
+                                                    second_context: &HashMap<K, V>)
+                                                    -> HashMap<K, V> {
+        let mut new_context = HashMap::new();
+        for (key, value) in first_context.iter() {
+            new_context.insert(*key, *value);
+        }
+        for (key, value) in second_context.iter() {
+            new_context.insert(*key, *value);
+        }
+        new_context
+    }
+
+
+    pub fn drop(&mut self, names: &[String]) -> Result<DataFrame<T>, &'static str> {
+        let mut idxs = vec![];
+
+        let new_map: &mut BTreeMap<String, usize> = &mut self.data_map.clone();
+        for name in names.iter() {
+            let idx = new_map.remove(name);
+            idxs.push(idx.unwrap());
+            for (_, y) in new_map.iter_mut() {
+                if y > &mut idx.unwrap() {
+                    *y -= 1;
+
+                }
+            }
+        }
+        let cols = self.shape().1;
+        let to_keep: Vec<usize> = (0..cols).filter(|x| !idxs.iter().any(|&y| y == *x)).collect();
+
+        DataFrame::new(self.inner_matrix.select(Axis(1), &to_keep[..]),
+                       new_map.to_owned())
+    }
+
+    pub fn concat(&self, axis: Axis, other: &DataFrame<T>) -> Result<DataFrame<T>, &'static str> {
+
+        match axis {
+            Axis(0) => {
+                if self.shape().1 == other.shape().1 {
+                    let new_map: BTreeMap<String, usize> = self.data_map
+                        .iter()
+                        .map(|(ref x, &y)| (x.to_string(), y))
+                        .collect();
+                    let other_idxs: Vec<usize> = other.data_map
+                        .values()
+                        .map(|x| *x)
+                        .collect();
+                    let other_matrix = other.inner_matrix.select(Axis(1), &other_idxs[..]);
+                    let new_matrix =
+                        match stack(Axis(0), &[self.inner_matrix.view(), other_matrix.view()]) {
+                            Ok(z) => z,
+                            Err(_) => return Err("could not insert into matrix."),
+                        };
+                    DataFrame::new(new_matrix, new_map)
+                } else {
+                    return Err("DataFrame column dimensions do not match.");
+                }
+            }
+            Axis(1) => {
+                if self.shape().0 == other.shape().0 {
+                    let other_map: BTreeMap<String, usize> = other.data_map
+                        .iter()
+                        .map(|(x, y)| (x.to_string(), y + self.data_map.len()))
+                        .collect();
+                    let new_map: BTreeMap<String, usize> = self.data_map
+                        .iter()
+                        .chain(other_map.iter())
+                        .map(|(x, y)| (x.to_string(), *y))
+                        .collect();
+
+                    let new_matrix = match stack(Axis(1),
+                                                 &[self.inner_matrix.view(),
+                                                   other.inner_matrix.view()]) {
+                        Ok(z) => z,
+                        Err(_) => return Err("could not insert into matrix."),
+                    };
+                    DataFrame::new(new_matrix, new_map)
+                } else {
+                    return Err("DataFrame row dimensions do not match.");
+                }
+            }
+            _ => Err("invalid axis"),
+        }
+
+    }
+    pub fn inner_join(&self, other: &DataFrame<T>, on: &str) -> Result<DataFrame<T>, &'static str> {
 
         let h: HashMap<Value, usize> = {
             let h_idx: &usize = match self.data_map
@@ -119,14 +209,21 @@ impl<T: Float> DataFrame<T> {
             }
 
         };
+        let n2: BTreeMap<String, usize> = other.data_map
+            .iter()
+            .map(|(x, y)| (x.to_string() + "_x", *y + self.data_map.len()))
+            .collect();
 
-        let mut n1: Vec<String> = self.data_map.keys().map(|x| x.clone()).collect();
-        let n2: Vec<String> = other.data_map.keys().map(|x| x.clone() + "_x").collect();
-        n1.extend_from_slice(&n2[..]);
 
+        let n1: BTreeMap<String, usize> = self.data_map
+            .iter()
+            .chain(n2.iter())
+            .map(|(x, y)| (x.to_string(), *y))
+            .collect();
 
-        println!("join names shape - {:?}", n1.len());
-        println!("join matrix shape - {:?}", new_matrix.shape()[1]);
+        // println!("{:?}", n1);
+        // println!("join names shape - {:?}", n1.len());
+        // println!("join matrix shape - {:?}", new_matrix.shape()[1]);
         DataFrame::new(new_matrix, n1)
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -11,7 +11,7 @@ pub mod tests {
     use ndarray_rand::RandomExt;
     use std::rc::Rc;
     use rand::{thread_rng, Rng};
-    use std::collections::HashMap;
+    use std::collections::{HashMap, BTreeMap};
     #[test]
     fn outer_left_join() {
         let mut left = HashMap::new();
@@ -48,7 +48,9 @@ pub mod tests {
     #[test]
     fn dataframe_creation() {
         let a = arr2(&[[2., 3.], [3., 4.]]);
-        let names = vec!["a", "b"].iter().map(|x| x.to_string()).collect();
+        let mut names: BTreeMap<String, usize> = BTreeMap::new();
+        names.insert("a".to_string(), 0);
+        names.insert("b".to_string(), 1);
         let df = DataFrame::new(a, names);
         assert!(df.is_ok())
     }
@@ -56,10 +58,10 @@ pub mod tests {
     #[test]
     fn dataframe_index() {
         let a = arr2(&[[2., 3.], [3., 4.]]);
-        let names = vec!["a", "b"].iter().map(|x| x.to_string()).collect();
-        let df = {
-            DataFrame::new(a, names).unwrap()
-        };
+        let mut names: BTreeMap<String, usize> = BTreeMap::new();
+        names.insert("a".to_string(), 0);
+        names.insert("b".to_string(), 1);
+        let df = DataFrame::new(a, names).unwrap();
         assert!(df.get("a".to_string()) == Ok(arr2(&[[2., 3.], [3., 4.]]).column(0).to_owned()))
     }
 
@@ -73,18 +75,38 @@ pub mod tests {
         let c = stack(Axis(1), &[a.view(), b.view()]).unwrap();
         let e = stack(Axis(1), &[d.view(), f.view()]).unwrap();
 
-        let c_names: Vec<String> = vec!["1", "2", "3"].iter().map(|x| x.to_string()).collect();
-        let e_names: Vec<String> = vec!["4", "5", "3"].iter().map(|x| x.to_string()).collect();
+        let mut c_names: BTreeMap<String, usize> = BTreeMap::new();
+        c_names.insert("1".to_string(), 0);
+        c_names.insert("2".to_string(), 1);
+        c_names.insert("3".to_string(), 2);
+
+        let mut e_names: BTreeMap<String, usize> = BTreeMap::new();
+        e_names.insert("4".to_string(), 0);
+        e_names.insert("5".to_string(), 1);
+        e_names.insert("3".to_string(), 2);
 
 
-        let c_df = DataFrame::new(c, c_names).unwrap();
-        let e_df = DataFrame::new(e, e_names).unwrap();
 
-        println!("c - {:?}", c_df);
-        println!("e - {:?}", e_df);
+        let c_df = DataFrame::new(c.clone(), c_names).unwrap();
+        let e_df = DataFrame::new(e.clone(), e_names).unwrap();
 
-        println!("join - {:?}", c_df.inner_join(e_df, "3"));
 
+
+        let mut join_names: BTreeMap<String, usize> = BTreeMap::new();
+        join_names.insert("1".to_string(), 0);
+        join_names.insert("2".to_string(), 1);
+        join_names.insert("3".to_string(), 2);
+        join_names.insert("4_x".to_string(), 3);
+        join_names.insert("5_x".to_string(), 4);
+        join_names.insert("3_x".to_string(), 5);
+        let join_matrix = stack(Axis(1),
+                                &[c.select(Axis(0), &[0, 2]).view(),
+                                  e.select(Axis(0), &[0, 2]).view()])
+            .unwrap();
+        let join_df = DataFrame::new(join_matrix, join_names);
+        let test_df = c_df.inner_join(&e_df, "3");
+        println!("{:?}", test_df.clone().unwrap().data_map);
+        assert_eq!(join_df.unwrap(), test_df.clone().unwrap())
 
     }
 
@@ -92,21 +114,77 @@ pub mod tests {
     #[test]
     fn dataframe_insert() {
         let a = arr2(&[[2., 3.], [3., 4.]]);
-        let names = vec!["a", "b"].iter().map(|x| x.to_string()).collect();
+        let mut names: BTreeMap<String, usize> = BTreeMap::new();
+        names.insert("a".to_string(), 0);
+        names.insert("b".to_string(), 1);
         let df = DataFrame::new(a, names).unwrap();
         let new_array = arr2(&[[5.], [6.]]);
 
         let new_df = df.insert(new_array, "c".to_string());
-        let new_names: Vec<String> = vec!["a", "b", "c"].iter().map(|x| x.to_string()).collect();
+        let mut new_names: BTreeMap<String, usize> = BTreeMap::new();
+        new_names.insert("a".to_string(), 0);
+        new_names.insert("b".to_string(), 1);
+        new_names.insert("c".to_string(), 2);
         let a_prime = arr2(&[[2., 3., 5.], [3., 4., 6.]]);
 
         assert_eq!(DataFrame::new(a_prime, new_names).unwrap(), new_df.unwrap())
     }
 
     #[test]
+    fn dataframe_concat() {
+        let a = arr2(&[[2., 3.], [3., 4.]]);
+        let b = arr2(&[[7., 1.], [7., 6.]]);
+        let mut a_names: BTreeMap<String, usize> = BTreeMap::new();
+        a_names.insert("a".to_string(), 0);
+        a_names.insert("b".to_string(), 1);
+
+        let mut b_names: BTreeMap<String, usize> = BTreeMap::new();
+        b_names.insert("c".to_string(), 0);
+        b_names.insert("d".to_string(), 1);
+        let df = DataFrame::new(a, a_names).unwrap();
+        let df_1 = DataFrame::new(b, b_names).unwrap();
+
+        let row_concat = df.concat(Axis(0), &df_1);
+        let col_concat = df.concat(Axis(1), &df_1);
+
+
+        let mut a_prime_names: BTreeMap<String, usize> = BTreeMap::new();
+        a_prime_names.insert("a".to_string(), 0);
+        a_prime_names.insert("b".to_string(), 1);
+        let mut b_prime_names: BTreeMap<String, usize> = BTreeMap::new();
+        b_prime_names.insert("a".to_string(), 0);
+        b_prime_names.insert("b".to_string(), 1);
+        b_prime_names.insert("c".to_string(), 2);
+        b_prime_names.insert("d".to_string(), 3);
+        let a_prime = arr2(&[[2., 3.], [3., 4.], [7., 1.], [7., 6.]]);
+        let b_prime = arr2(&[[2., 3., 7., 1.], [3., 4., 7., 6.]]);
+
+        assert_eq!(DataFrame::new(a_prime, a_prime_names).unwrap(),
+                   row_concat.unwrap());
+        assert_eq!(DataFrame::new(b_prime, b_prime_names).unwrap(),
+                   col_concat.unwrap())
+    }
+
+    #[test]
+    fn dataframe_drop() {
+        let a = arr2(&[[2., 3.], [3., 4.]]);
+        let mut names: BTreeMap<String, usize> = BTreeMap::new();
+        names.insert("a".to_string(), 0);
+        names.insert("b".to_string(), 1);
+        let mut df = DataFrame::new(a, names).unwrap();
+        let new_df = df.drop(&["a".to_string()]);
+        let mut new_names: BTreeMap<String, usize> = BTreeMap::new();
+        new_names.insert("b".to_string(), 0);
+        let a_prime = arr2(&[[3.], [4.]]);
+        assert_eq!(DataFrame::new(a_prime, new_names).unwrap(), new_df.unwrap())
+    }
+
+    #[test]
     fn dataframe_creation_failure() {
         let a = Array::random((2, 5), Range::new(0., 10.));
-        let names = vec!["1", "2"].iter().map(|x| x.to_string()).collect();
+        let mut names: BTreeMap<String, usize> = BTreeMap::new();
+        names.insert("1".to_string(), 0);
+        names.insert("2".to_string(), 1);
         let df = DataFrame::new(a, names);
         assert!(df.is_err())
     }
@@ -116,8 +194,12 @@ pub mod tests {
     #[bench]
     fn bench_creation(b: &mut Bencher) {
         let a = Array::random((10, 5), Range::new(0., 10.));
-        let names: Vec<String> =
-            vec!["1", "2", "3", "4", "5"].iter().map(|x| x.to_string()).collect();
+        let mut names: BTreeMap<String, usize> = BTreeMap::new();
+        names.insert("1".to_string(), 0);
+        names.insert("2".to_string(), 1);
+        names.insert("3".to_string(), 2);
+        names.insert("4".to_string(), 3);
+        names.insert("5".to_string(), 4);
         b.iter(|| DataFrame::new(a.clone(), names.clone()));
     }
 
@@ -125,27 +207,34 @@ pub mod tests {
 
     #[bench]
     fn bench_inner_join(b: &mut Bencher) {
-        let a = Array::random((2000, 10), Range::new(0., 10.));
-        let z = Array::random((2000, 1), Range::new(0., 10.));
-        let d = Array::random((2000, 10), Range::new(0., 10.));
+        let a = Array::random((20000, 10), Range::new(0., 10.));
+        let z = Array::random((20000, 1), Range::new(0., 10.));
+        let d = Array::random((20000, 10), Range::new(0., 10.));
         let c = stack(Axis(1), &[a.view(), z.view()]).unwrap();
         let e = stack(Axis(1), &[d.view(), z.view()]).unwrap();
 
 
-        let c_names: Vec<String> = (0..11).map(|x| x.to_string()).collect();
-        let e_names: Vec<String> = (0..11).map(|x| x.to_string()).collect();
+        let mut c_names: BTreeMap<String, usize> = BTreeMap::new();
+
+        for i in 0..11 {
+            c_names.insert(i.to_string(), i);
+        }
+        let mut e_names: BTreeMap<String, usize> = BTreeMap::new();
+        for i in 0..11 {
+            e_names.insert(i.to_string(), i);
+        }
 
 
         let c_df = DataFrame::new(c, c_names).unwrap();
         let e_df = DataFrame::new(e, e_names).unwrap();
 
-        b.iter(|| c_df.clone().inner_join(e_df.clone(), "9"));
+        b.iter(|| c_df.inner_join(&e_df, "9"));
     }
 
     #[bench]
     fn bench_stack(b: &mut Bencher) {
-        let a = Array::random((2000, 10), Range::new(0., 10.));
-        let d = Array::random((2000, 10), Range::new(0., 10.));
+        let a = Array::random((20000, 10), Range::new(0., 10.));
+        let d = Array::random((20000, 10), Range::new(0., 10.));
 
 
         b.iter(|| stack(Axis(1), &[a.view(), d.view()]));
@@ -154,12 +243,12 @@ pub mod tests {
     #[bench]
     fn bench_inner_join_bare(b: &mut Bencher) {
         let mut left = HashMap::new();
-        for (i, j) in (1..2000).zip((1..2000).map(|x| x.to_string())) {
+        for (i, j) in (1..20000).zip((1..20000).map(|x| x.to_string())) {
             left.insert(i, j);
         }
 
         let mut right = HashMap::new();
-        for (i, j) in (1993..4000).zip((1993..4000).map(|x| x.to_string())) {
+        for (i, j) in (19993..40000).zip((19993..40000).map(|x| x.to_string())) {
             right.insert(i, j);
         }
 
