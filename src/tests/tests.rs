9550f2583ab69f3e607d0d24a54a98936a758e6c
pegasos1 <sgrangan01@gmail.com>
2016-12-26 15:06:42 -0800

    added transform tests

--- src/tests/tests.rs
+++ src/tests/tests.rs
@@ -64,7 +64,14 @@ fn dataframe_creation() {
 }
 
 #[test]
-fn dataframe_index() {
+fn dataframe_creation_failure() {
+    let a = Array::random((2, 5), Range::new(0., 10.));
+    let df: Result<DataFrame<f64, String>> = DataFrame::new(a).columns(&["1", "2"]);
+    assert!(df.is_err())
+}
+
+#[test]
+fn dataframe_select() {
     let a = arr2(&[[2., 3.], [3., 4.]]);
     let df: DataFrame<f64, String> = DataFrame::new(a).columns(&["a", "b"]).unwrap();
     let select_idx = vec!["a"];
@@ -85,9 +92,78 @@ fn dataframe_index() {
         .unwrap();
     assert_eq!(z.unwrap(), expected);
 }
+
 #[test]
-fn dataframe_creation_failure() {
-    let a = Array::random((2, 5), Range::new(0., 10.));
-    let df: Result<DataFrame<f64, String>> = DataFrame::new(a).columns(&["1", "2"]);
-    assert!(df.is_err())
+fn dataframe_remove() {
+    let a = arr2(&[[2., 3.], [3., 4.]]);
+    let df: DataFrame<f64, String> = DataFrame::new(a).columns(&["a", "b"]).unwrap();
+    let remove_idx = vec!["a"];
+    let z = df.remove(&remove_idx[..], UtahAxis::Column).as_df();
+    let col = df.data.column(1).clone();
+    let expected = DataFrame::from_array(col.to_owned(), UtahAxis::Column)
+        .columns(&["b"])
+        .unwrap();
+    assert_eq!(z.unwrap(), expected);
+
+    let remove_idx = vec!["0"];
+    let z = df.remove(&remove_idx[..], UtahAxis::Row).as_df();
+    let row = df.data.row(1).clone();
+    let expected = DataFrame::from_array(row.to_owned(), UtahAxis::Row)
+        .index(&["1"])
+        .unwrap()
+        .columns(&["a", "b"])
+        .unwrap();
+    assert_eq!(z.unwrap(), expected);
+}
+
+#[test]
+fn dataframe_append() {
+    {
+        let a = arr2(&[[2., 3.], [3., 4.]]);
+        let mut df: DataFrame<f64, String> = DataFrame::new(a).columns(&["a", "b"]).unwrap();
+        let col = df.select(&["a"], UtahAxis::Column).as_array().unwrap();
+        let z = df.append("c", col.view(), UtahAxis::Column).as_df();
+        let b = arr2(&[[2., 3., 2.], [3., 3., 4.]]);
+        let expected = DataFrame::new(b)
+            .columns(&["c", "a", "b"])
+            .unwrap();
+        assert_eq!(z.unwrap(), expected);
+    }
+    {
+        let a = arr2(&[[2., 3.], [3., 4.]]);
+        let mut df: DataFrame<f64, String> = DataFrame::new(a).columns(&["a", "b"]).unwrap();
+        let col = df.select(&["1"], UtahAxis::Row).as_array().unwrap();
+        let z = df.append("2", col.view(), UtahAxis::Row).as_df();
+        let b = arr2(&[[3., 4.], [2., 3.], [3., 4.]]);
+        let expected = DataFrame::new(b)
+            .columns(&["a", "b"])
+            .unwrap()
+            .index(&["2", "0", "1"])
+            .unwrap();
+        assert_eq!(z.unwrap(), expected);
+    }
+}
+
+#[test]
+fn dataframe_mapdf() {
+    {
+        let a = arr2(&[[2., 3.], [3., 4.]]);
+        let mut df: DataFrame<f64, String> = DataFrame::new(a).columns(&["a", "b"]).unwrap();
+        let z: Vec<(String, Row<f64>)> = df.map(|x| x * 2.0, UtahAxis::Row).collect();
+        let b = arr2(&[[4., 6.], [6., 8.]]);
+        let mut expected = Vec::new();
+        expected.push(("0".to_string(), b.row(0).to_owned()));
+        expected.push(("1".to_string(), b.row(1).to_owned()));
+        assert_eq!(z, expected);
+    }
+    {
+        let a = arr2(&[[2., 3.], [3., 4.]]);
+        let mut df: DataFrame<f64, String> = DataFrame::new(a).columns(&["a", "b"]).unwrap();
+        let z: Vec<(String, Row<f64>)> = df.map(|x| x * 2.0, UtahAxis::Column).collect();
+        let b = arr2(&[[4., 6.], [6., 8.]]);
+        let mut expected = Vec::new();
+        expected.push(("a".to_string(), b.row(0).to_owned()));
+        expected.push(("b".to_string(), b.row(1).to_owned()));
+        assert_eq!(z, expected);
+    }
 }
